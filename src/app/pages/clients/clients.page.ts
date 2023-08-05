fa2121c7fa8d4e75991a6bd7f6006beed0ddc9f9
Reich.arm <rmariasp@puce.edu.ec>
2023-08-04 20:35:29 -0500

    Apartado de Listas

--- src/app/pages/clients/clients.page.ts
+++ src/app/pages/clients/clients.page.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.page.html',
   styleUrls: ['./clients.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, DragDropModule]
 })
 export class ClientsPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  } 
+
+  move_to_map(){
+    this.router.navigate(['/map'])
+  }
+//Funcion pa mover las cositas
+  onDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
   }
 
 }
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
