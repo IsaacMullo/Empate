ad8b124e14c5a0c65c3099788dc090aae149edec
Isaac Mullo <aimullo@puce.edu.ec>
2023-09-10 22:59:27 -0500

    Actulización Rama

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
-@Component({
-  selector: 'app-map',
-  templateUrl: './map.page.html',
-  styleUrls: ['./map.page.scss'],
-})
+
 interface PostIt {
   x: number;
   y: number;
   content: string;
 }
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, DragDropModule]
+})
 
 export class MapPage implements OnInit {
   postIts: PostIt[]=[];
